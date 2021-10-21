f0d8de711141b08f5fceee5c65865c5cb92c43ef
apradillo26 <player3rdy@gmail.com>
2021-10-21 23:57:50 +0200

    commit prueba tag

--- src/main/java/uno.java
+++ src/main/java/uno.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class uno {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("hola mundo");
         
     }
 }
