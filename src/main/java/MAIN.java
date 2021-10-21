4c8491fd3ed4800718ac4831f77e3c8640e32b95
apradillo26 <player3rdy@gmail.com>
2021-10-22 01:13:49 +0200

    commit develop2

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
-public class dos {
+public class MAIN {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("prueba_tag");
+
+        System.out.println("cambios desde develop2");
 
     }
 }
