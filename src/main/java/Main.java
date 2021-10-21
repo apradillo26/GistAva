9b22db3cb0c41ca2f9e279a4a3a258501d09a8e1
apradillo26 <player3rdy@gmail.com>
2021-10-22 00:49:35 +0200

    commit DEVELOP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 
-public class dos {
+public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("prueba_tag2");
+
+        System.out.println("nuevo en branch DEVELOP");
+
+
+
 
     }
 }
