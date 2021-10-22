c684f3510b58f164975638cdf976b15043a15e46
apradillo26 <player3rdy@gmail.com>
2021-10-22 02:17:41 +0200

    commit develop2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-class MAIN {
+class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("prueba_tag");
+        System.out.println("prueba_tag2");
 
         System.out.println("nuevo en branch DEVELOP");
         System.out.println("cambios en develop");
 
-        System.out.println("cambios desde develop2, mas");
+
 
     }
 }
